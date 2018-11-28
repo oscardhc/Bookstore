eb38c695c1618646ed80f510f3f3326e26b1a131
oscardhc <oscardhc@sjtu.edu.cn>
2018-11-28 10:32:01 +0800

    Initial Commit

---
+++ Bookstore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bookstore
+//
+//  Created by Haichen Dong on 11/28/18.
+//  Copyright © 2018 Haichen Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
