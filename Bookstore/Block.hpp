67f5c4a403ab69c65a14e771da7fb81c754db8d8
oscardhc <oscardhc@sjtu.edu.cn>
2018-12-13 23:50:57 +0800

    optimize

--- Bookstore/Block.hpp
+++ Bookstore/Block.hpp
@@ -22,12 +22,17 @@ const int sqrSize = 900;
 class Block {
 public:
     int minVal, maxVal;
-    int nxt;
-    int tot, key[sqrSize], id[sqrSize];
+    int nxt, tot, val[sqrSize];
     void reset() {
         tot = 0;
         minVal = INT_MAX;
         maxVal = INT_MIN;
+    }
+    inline int& key(int i) {
+        return val[i << 1];
+    }
+    inline int& id(int i) {
+        return val[(i << 1) + 1];
     }
 };
 
@@ -47,7 +52,7 @@ public:
     }
     Blocks() {
     }
-    void init (std::string _filename) {
+    void init(std::string _filename) {
         filename = _filename;
         file = std::fstream(filename, std::ios::app);
         file.close();
@@ -74,7 +79,7 @@ public:
             file.read((char*)&tmp, BlockSize);
             printf("BLOCK %d (%d) tot:%d max:%d min:%d nx:%d\n", cur, i, tmp.tot, tmp.maxVal, tmp.minVal, tmp.nxt);
             for (int j = 0; j < tmp.tot; j++) {
-                printf("            %d %d\n", tmp.key[j], tmp.id[j]);
+                printf("            %d %d\n", tmp.key(j), tmp.id(j));
             }
             cur = tmp.nxt;
         }
@@ -94,18 +99,18 @@ public:
                 cur = tmp.nxt;
                 continue;
             } else {
-                file.seekp(-4 * sizeof(int), std::ios::cur);
-                file.read((char*)&tmp, BlockSize);
+                file.seekp(BaseSize + cur * BlockSize, std::ios::beg);
+                file.read((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                 tmp.tot++;
-                tmp.key[tmp.tot - 1] = key;
-                tmp.id[tmp.tot - 1] = id;
+                tmp.key(tmp.tot - 1) = key;
+                tmp.id(tmp.tot - 1) = id;
                 tmp.maxVal = std::max(tmp.maxVal, key);
                 tmp.minVal = std::min(tmp.minVal, key);
                 if (tmp.tot == sqrSize) {
                     std::pair<int, int> mda[sqrSize];
                     for (int j = 0; j < sqrSize; j++) {
-                        mda[j].first = tmp.key[j];
-                        mda[j].second = tmp.id[j];
+                        mda[j].first = tmp.key(j);
+                        mda[j].second = tmp.id(j);
                     }
                     std::sort(mda, mda + sqrSize);
                     Block blk;
@@ -113,27 +118,27 @@ public:
                     blk.reset();
                     for (int j = 0; j < sqrSize; j++) {
                         if (j < sqrSize / 2) {
-                            tmp.key[tmp.tot] = mda[j].first;
-                            tmp.id[tmp.tot] = mda[j].second;
-                            tmp.minVal = std::min(tmp.minVal, tmp.key[j]);
-                            tmp.maxVal = std::max(tmp.maxVal, tmp.key[j]);
+                            tmp.key(tmp.tot) = mda[j].first;
+                            tmp.id(tmp.tot) = mda[j].second;
+                            tmp.minVal = std::min(tmp.minVal, tmp.key(j));
+                            tmp.maxVal = std::max(tmp.maxVal, tmp.key(j));
                             tmp.tot++;
                         } else {
-                            blk.key[blk.tot] = mda[j].first;
-                            blk.id[blk.tot] = mda[j].second;
-                            blk.minVal = std::min(blk.minVal, blk.key[j]);
-                            blk.maxVal = std::max(blk.maxVal, blk.key[j]);
+                            blk.key(blk.tot) = mda[j].first;
+                            blk.id(blk.tot) = mda[j].second;
+                            blk.minVal = std::min(blk.minVal, blk.key(j));
+                            blk.maxVal = std::max(blk.maxVal, blk.key(j));
                             blk.tot++;
                         }
                     }
                     blk.nxt = tmp.nxt;
                     tmp.nxt = tot;
                     file.seekp(-BlockSize, std::ios::cur);
-                    file.write((char*)&tmp, BlockSize);
+                    file.write((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                     addBlock(&blk);
                 } else {
                     file.seekp(BaseSize + cur * BlockSize, std::ios::beg);
-                    file.write((char*)&tmp, BlockSize);
+                    file.write((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                 }
                 break;
             }
@@ -148,10 +153,10 @@ public:
             file.read((char*)&tmp, 4 * sizeof(int));
             if (tmp.tot > 0 && tmp.minVal <= key && tmp.maxVal >= key) {
                 file.seekp(BaseSize + cur * BlockSize, std::ios::beg);
-                file.read((char*)&tmp, BlockSize);
+                file.read((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                 for (int j = 0; j < tmp.tot; j++) {
-                    if (tmp.key[j] == key && tmp.id[j] != -1) {
-                        ret.push_back(tmp.id[j]);
+                    if (tmp.key(j) == key && tmp.id(j) != -1) {
+                        ret.push_back(tmp.id(j));
                     }
                 }
                 if (i == tot - 1 || tmp.maxVal > key) {
@@ -169,18 +174,18 @@ public:
             file.read((char*)&tmp, 4 * sizeof(int));
             if (tmp.tot > 0 && tmp.minVal <= key && tmp.maxVal >= key) {
                 file.seekp(-4 * sizeof(int), std::ios::cur);
-                file.read((char*)&tmp, BlockSize);
+                file.read((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                 for (int j = 0; j < tmp.tot; j++) {
-                    if (tmp.key[j] == key && tmp.id[j] == id) {
+                    if (tmp.key(j) == key && tmp.id(j) == id) {
                         if (tmp.tot == 1) {
-                            tmp.id[j] = -1;
+                            tmp.id(j) = -1;
                         } else if (tmp.tot > 1) {
                             tmp.tot--;
-                            std::swap(tmp.key[j], tmp.key[tmp.tot]);
-                            std::swap(tmp.id[j], tmp.id[tmp.tot]);
+                            std::swap(tmp.key(j), tmp.key(tmp.tot));
+                            std::swap(tmp.id(j), tmp.id(tmp.tot));
                         }
                         file.seekp(-BlockSize, std::ios::cur);
-                        file.write((char*)&tmp, BlockSize);
+                        file.write((char*)&tmp, (4 + tmp.tot * 2) * sizeof(int));
                         return;
                     }
                 }
